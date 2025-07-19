6c39d88910eea095cffe82ee7d8d25478821a4b8
CSESCEM <office.cs@sahyadri.edu.in>
2025-07-19 16:17:19 +0530

    vite

---
+++ React/second_react_app/src/Third.jsx
@@ -0,0 +1,14 @@
+function Third(){
+    return(
+        <>
+            <h3>
+                My Third Component
+            </h3>
+            <p>
+                Paragraph in Third Component created inside second react app with Vite
+            </p>
+        </>
+    )
+}
+
+export default Third;
